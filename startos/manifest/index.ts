66ab57a1a9319640634a9bfc5f73e81b132381b0
Matt Hill <9935159+MattDHill@users.noreply.github.com>
2026-08-16 22:49:51 -0600

    style: format the i18n, manifest, and version files with prettier

--- startos/manifest/index.ts
+++ startos/manifest/index.ts
@@ -9,9 +9,7 @@ export const manifest = setupManifest({
   upstreamRepo: 'https://github.com/PrimalHQ/primal-web-app',
   marketingUrl: 'https://primal.net/',
   donationUrl: 'https://primal.net/',
-  docsUrls: [
-    'https://github.com/PrimalHQ/primal-web-app/blob/main/README.md',
-  ],
+  docsUrls: ['https://github.com/PrimalHQ/primal-web-app/blob/main/README.md'],
   description: { short, long },
   volumes: ['main'],
   images: {
